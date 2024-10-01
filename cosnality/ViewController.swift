5cf4f2f680914caeac075242590b7e691673b721
tansxx <kheej0404@naver.com>
2024-10-01 17:39:53 +0900

    Initial Commit

---
+++ cosnality/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cosnality
+//
+//  Created by 강희정 on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ cosnalityTests/cosnalityTests.swift
@@ -0,0 +1,17 @@
+//
+//  cosnalityTests.swift
+//  cosnalityTests
+//
+//  Created by 강희정 on 10/1/24.
+//
+
+import Testing
+@testable import cosnality
+
+struct cosnalityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
